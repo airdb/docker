d6f8da0f65d985687fcdafb90cc4bdb545ffdbe8
yino <m15829090357@163.com>
2021-08-25 19:37:23 +0800

    docker mysql8

--- mysql-8/init.sql
+++ mysql-8/init.sql
@@ -1,8 +1,6 @@
 update mysql.user set host = '%' where user = 'root' and host='localhost';
 update mysql.user set plugin='mysql_native_password' where user='root';
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
-
-FLUSH PRIVILEGES;
 CREATE USER 'airdb'@'%' IDENTIFIED BY 'airdb';
 GRANT ALL ON *.* TO 'airdb'@'%';
 
