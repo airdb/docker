800ac94484383f75cd1bdf08b74edc3bc97dfd2f
bumu <bumu@airdb.io>
2023-12-14 17:21:23 +0800

    update

--- python-uwsgi/hey.py
+++ python-uwsgi/hey.py
@@ -1,12 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    print(user_agent)
-    return f'Your User Agent is: {user_agent}'
+    #print(request.remote_addr, user_agent)
+    #return f'IP: {request.remote_addr} \n User Agent is: {user_agent}'
+
+    data = {
+        "ip": request.remote_addr,
+        "useragent": user_agent,
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
